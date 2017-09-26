ae94db4a71f189c94d4a3becdb3d5146faeda4e2
John Thomas <thomas07@vt.edu>
2017-09-25 20:05:08 -0700

    Loading files propery for simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV['PATH'] = "#{Dir.pwd}/bin#{File::PATH_SEPARATOR}#{ENV['PATH']}"
 require 'bundler/setup'
 require 'aruba'
 require 'aruba/api'
-require 'mcli'
 require 'pry'
 
 require 'simplecov'
@@ -11,6 +10,9 @@ SimpleCov.start
 require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
+SimpleCov.refuse_coverage_drop
+
+require 'mcli'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
